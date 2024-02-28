5266e8437ab2958688424a7fb0350f988750aa7f
shakib2123 <mshakibalhasan21@gmail.com>
2024-02-28 17:05:49 +0600

    sidebar routes created

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+
+      <Sidebar/>
+      {/* <MobileNav/> */}
+
       <div className="root-container">
-              <div className="wrapper">
-                  {children}
-              </div>
+        <div className="wrapper">{children}</div>
       </div>
     </main>
   );
