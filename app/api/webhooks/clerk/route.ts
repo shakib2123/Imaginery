a9ac7023cd985ee0c14f0b82e97d8de22329ac8c
shakib2123 <mshakibalhasan21@gmail.com>
2024-03-01 15:03:15 +0600

    custom form field created

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -56,10 +56,11 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
+  console.log("hello event:", eventType);
   // CREATE
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, username } =
+      evt.data;
 
     const user = {
       clerkId: id,
