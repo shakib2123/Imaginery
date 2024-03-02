709b0bbfdbae54a559db51b3a1d978291b3a78ed
shakib2123 <mshakibalhasan21@gmail.com>
2024-03-03 00:46:02 +0600

    bg remove and image restore implimented

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -25,11 +25,12 @@ import {
 import { Input } from "@/components/ui/input";
 import {
   aspectRatioOptions,
+  creditFee,
   defaultValues,
   transformationTypes,
 } from "@/constants";
 import { CustomField } from "./CustomField";
-import { useState, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
@@ -37,6 +38,7 @@ import TransformedImage from "./TransformedImage";
 import { addImage, updateImage } from "@/lib/actions/image.actions";
 import { getCldImageUrl } from "next-cloudinary";
 import { useRouter } from "next/navigation";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
 export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
@@ -183,7 +185,6 @@ const TransformationForm = ({
     return onChangeField(value);
   };
 
-  // TODO: Credit Fee to something else
   const onTransformHandler = async () => {
     setIsTransforming(true);
 
@@ -194,13 +195,21 @@ const TransformationForm = ({
     setNewTransformation(null);
 
     startTransition(async () => {
-      await updateCredits(userId, -1);
+      await updateCredits(userId, creditFee);
     });
   };
+
+
+  useEffect(() => {
+    if(image && (type === 'restore' || type === 'removeBackground')) {
+      setNewTransformation(transformationType.config)
+    }
+  }, [image, transformationType.config, type])
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField
           control={form.control}
           name="title"
